1796d616408c8fb363c2ae45b631c4697bb7874f
Taya <taya@infosechome.info>
2018-10-18 11:47:47 -0700

    network added

--- resources.tf
+++ resources.tf
@@ -1,4 +1,11 @@
 resource "google_compute_network" "our_development_network" {
   name = "devnetwork"
-  auto_create_subnetworks = true 
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "dev-subnet" {
+  ip_cidr_range = "10.0.1.0/24"
+  name = "devsubnet"
+  network = "${google_compute_network.our_development_network.self_link}"
+  region = "us-west1"
 }
